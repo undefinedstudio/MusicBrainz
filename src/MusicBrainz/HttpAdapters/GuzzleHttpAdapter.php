f3e8377a4630b34468b87d24fed22c211c6f43ae
Los <luca.horn@gmail.com>
2015-07-23 03:57:12 +0200

    json decode

--- src/MusicBrainz/HttpAdapters/GuzzleHttpAdapter.php
+++ src/MusicBrainz/HttpAdapters/GuzzleHttpAdapter.php
@@ -61,7 +61,7 @@ class GuzzleHttpAdapter extends AbstractHttpAdapter
             }
         }
         $response = $this->client->get($path, $options);
-        return $response->getBody()->getContents();
+        return json_decode($response->getBody()->getContents());
 /*
         $this->client->setBaseUrl(self::URL);
         $this->client->setConfig(
